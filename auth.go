46e5ec1101b5d824aae6c57c3e1c1890f8657ec5
viktigpetterr <viktor.pettersson@dreamdata.io>
2024-11-27 16:13:59 +0100

    Create revoke method

--- auth.go
+++ auth.go
@@ -2,6 +2,8 @@ package facebook
 
 import (
 	"context"
+	"errors"
+	"github.com/dreamdata-io/facebook/internal"
 	"golang.org/x/oauth2"
 )
 
@@ -12,6 +14,8 @@ type AuthClient interface {
 	AuthCodeURL(ctx context.Context, state string, options ...oauth2.AuthCodeOption) string
 	ExchangeOAuth2Code(ctx context.Context, oauth2Code string) (*oauth2.Token, error)
 	AccessToken(ctx context.Context, refreshToken string) (*oauth2.Token, error)
+	// Revoke revokes the token, this method requires authorization
+	Revoke(ctx context.Context, refreshToken string) error
 }
 
 type AuthOption func(*oauth2.Config)
@@ -67,4 +71,31 @@ func (c *Client) AccessToken(ctx context.Context, refreshToken string) (*oauth2.
 	}
 
 	return t, nil
+}
+
+func (c *Client) Revoke(_ context.Context, refreshToken string) error {
+	params := map[string]string{
+		"client_id":     c.oauth2Config.ClientID,
+		"client_secret": c.oauth2Config.ClientSecret,
+		"revoke_token":  refreshToken,
+	}
+
+	res, err := c.session.Get("/oauth/revoke", internal.MakeParams(params))
+	if err != nil {
+		return err
+	}
+
+	var revocation struct {
+		Success bool `json:"success"`
+	}
+
+	if err = res.Decode(&revocation); err != nil {
+		return err
+	}
+
+	if !revocation.Success {
+		return errors.New("failed to revoke token")
+	}
+
+	return nil
 }
